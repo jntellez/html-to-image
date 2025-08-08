e33975539bd3ed59be44312e3df0863d35a07a2a
Juan Tellez <juantellez916@gmail.com>
2025-08-07 22:53:48 -0600

    style Update Shadcn tabs and resizable design

---
+++ src/components/ui/resizable.tsx
@@ -0,0 +1,54 @@
+import * as React from "react"
+import { GripVerticalIcon } from "lucide-react"
+import * as ResizablePrimitive from "react-resizable-panels"
+
+import { cn } from "@/lib/utils"
+
+function ResizablePanelGroup({
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
+  return (
+    <ResizablePrimitive.PanelGroup
+      data-slot="resizable-panel-group"
+      className={cn(
+        "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function ResizablePanel({
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.Panel>) {
+  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />
+}
+
+function ResizableHandle({
+  withHandle,
+  className,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+  withHandle?: boolean
+}) {
+  return (
+    <ResizablePrimitive.PanelResizeHandle
+      data-slot="resizable-handle"
+      className={cn(
+        "bg-transparent flex active:bg-border hover:bg-border data-[panel-group-direction=horizontal]:mx-0.5 data-[panel-group-direction=vertical]:my-0.5 relative items-center justify-center focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 w-3 data-[panel-group-direction=vertical]:h-3 data-[panel-group-direction=vertical]:w-full",
+        className
+      )}
+      {...props}
+    >
+      {withHandle && (
+        <div className="bg-transparent flex active:bg-border hover:bg-border text-transparent active:text-white hover:text-white z-10 h-full w-full items-center justify-center rounded-xs border">
+          <GripVerticalIcon className="size-2.5" />
+        </div>
+      )}
+    </ResizablePrimitive.PanelResizeHandle>
+  )
+}
+
+export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
