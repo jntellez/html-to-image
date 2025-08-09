c11ef1c2774ba34c7c6ec82b7b8f2a17ac5a65de
Juan Tellez <juantellez916@gmail.com>
2025-08-09 00:22:28 -0600

    add Header

---
+++ src/components/header.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <header className="w-full border-b px-4 py-3 mb-2 flex items-center justify-between bg-background">
+      <Link to="/" className="text-lg font-semibold">
+        HTML to Image
+      </Link>
+
+      <nav className="flex items-center gap-2">
+        <Link to="/playground">
+          <Button variant="ghost">Playground</Button>
+        </Link>
+        <ModeToggle />
+      </nav>
+    </header>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@/components/theme-provider'
-import Home from '@/pages/index'
+import { Header } from './components/header'
 import Playground from "@/pages/playground"
+import Home from '@/pages/index'
 import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/playground" element={<Playground />} />
